ece578d0bf83ae93050b164bcc14b96b7d5cb810
Wraient Daemon <81632222+Wraient@users.noreply.github.com>
2021-09-01 16:01:32 +0530

    Update and rename mian.py to main.py
    there was a very small bug which made the application not work at all I fixed it!
    Open for collaboration contact @wraient.911 on instagram
    Use or modify as you want
    Thanks

--- Auto Factorisor/main.py
+++ Auto Factorisor/main.py
@@ -1,6 +1,6 @@
 import os
 import time
-
+time_Used = time.time()
 def mainFunc():
     def isInt(string):
         try:
@@ -11,27 +11,51 @@ def mainFunc():
 
     num = True
     while True:
-        # Uncomment below lines for knowing the time needed for the program 
-        # t1 = time.time()
+        # comment below appropriate lines for hiding the time used by the program
+        t1 = time.time()
         try:
             os.system("clear")
             if num == False:
                 print("Please enter a number.")
                 num = True
+            t2 = time.time()
             additional  = int(input("Enter the middle number : "))
+            t3 = time.time()
+            t4 = 0
             try:
                 os.system("clear")
                 print("Middle number : " + str(additional))
-                multiplier = int(input("Enter the product of first and last number : "))
+                t4 = time.time()
+                multiplier = input("Enter the product of first and last number : ")
+                t5 = time.time()
+                multiplier = int(multiplier)
+                t6 = time.time()
+                time_taken_by_computer = (t2-t1)+(t4-t3)+(t6-t5)
+                time_taken_by_user = (t3-t2)+(t5-t4)
             except ValueError:
+                t6 = time.time()
+                time_taken_by_computer = (t2-t1)+(t4-t3)+(t6-t5)
+                time_taken_by_user = (t5-t4)+(t3-t2)
+                t6 = 0
                 while True:
                     os.system("clear")
                     print("Please enter a number")
                     print("\nMiddle number : " + str(additional))
+                    if t6 == 0:
+                        t6 = time.time()
                     multiplier = input("Enter the product of first and last number : ")
+                    t7 = time.time()
                     if isInt(multiplier):
                         multiplier = int(multiplier)
+                        
+
+                        time_taken_by_computer = (t2-t1)+(t4-t3)+(t6-t5)
+                        time_taken_by_user = (t5-t4)+(t3-t2)+(t7-t6)
                         break
+            os.system("clear")
+            print("Middle number : " + str(additional))
+            print("Last number : " + str(multiplier) + "\n")
+            t8 = time.time()
             run = True
             found = False
         # Not very efficient but works fine for me (time and space complexity of O^2 or more I guess)
@@ -47,13 +71,17 @@ def mainFunc():
             if found == False:
                 print("\nno solution between 0 and 1000.")
             
-            # t2 = time.time()
-            # print("\nTime : " + str(t2-t1) + " seconds")
-            cmd = input("\nPress \"n\" to do other calculation and any other key to quit ")
+            t9 = time.time()
+            print(f"\nTime taken by user : {str(round(time_taken_by_user, 4))} seconds\nTime taken by CPU : {str(round(time_taken_by_computer+(t9-t8), 4))} seconds")
+            cmd = input("\nPress \"n\" to do other calculation and any other key to quit : ")
             if cmd.lower() == "n": continue
             else:
                 os.system("clear")
-                break
+                time_Used2 = time.time()
+                print("Used application for : " + str(time_Used2 - time_Used) + " seconds")
+                cmd1 = input("\nPress \"n\" to do other calculation and any other key to quit : ")
+                if cmd1.lower() == "n": continue
+                else: break
 
         except ValueError:
             num = False
